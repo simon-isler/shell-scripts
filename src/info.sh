ffecc3c82a89d869870e9233375b0e3e93e0b407
simon-isler <simon-isler@gmx.ch>
2018-12-04 10:35:31 +0100

    add info.sh

---
+++ src/info.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Programm: info.sh
+# Aufruf: ./info.sh Datei (Datei2)
+#
+# Skript für die Ausgabe der Zugriffsrechte auf die Datei in Form dreier Oktalzahlen (z.B. 754)
+#
+# Autor: Simon Isler
+# Version: 1.0
+# Datum: 04.12.2018
+
+befehl="$0 Datei (Datei2)"
+
+# keine Argumente
+
+if [ $# -eq 0 ] ; then
+	echo $befehl
+	exit 1
+fi
+
+# ein Argument
+if [ $# -eq 1 ] ; then
+	# Argument = help
+	if [ $1 = -h -o $1 = -help ] ; then
+		echo "$0 gibt die Zugriffsrechte auf die angegebene Datei in Form dreier Oktalzahlen (z.B. 754) aus."
+		echo "Richtiger Syntax: " $befehl
+	# Datei nicht vorhanden
+	elif [ ! -e $1 ] ; then
+		echo $1 existiert nicht
+		exit 2
+	else
+		stat -f %A $1
+	fi
+	exit 0
+fi
+
+# zwei Argumente
+if [ $# -eq 2 ] ; then
+	# 1. Argument ist keine Datei
+	if [ ! -e $1 ] ; then
+		echo $1 existiert nicht
+		exit 2
+	# 2. Datei bzw. Argument existiert
+	elif [ -e $2 ] ; then
+		read -p "Möchten Sie die bestehende Datei überschreiben? (0), den Output an die bestehende Datei anhängen? (1) oder abbrechen? (3)" action
+			case $antwort in
+			1) cat $1 > $2; echo "Die Datei wurde überschrieben.";;
+			2) cat $1 >> $2; echo "Die Datei wurde angehängt.";;
+			*) echo "Abgebrochen!";;
+			esac
+		exit 0
+
+	# Zugriffsrechte ausgeben
+	else
+		stat -f %A $1
+	fi
+	exit 0
+fi
+
+# zu viele Argumente
+if [ $# -gt 2 ] ; then
+	echo "Es wurden zu viele Argumente angegeben!";
+	echo "Richtiger Syntax: " $befehl
+	exit 3
+fi
