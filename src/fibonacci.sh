462787f2d4fdeaadb3d951c1865ed99e34acc2aa
simon-isler <simon-isler@gmx.ch>
2018-12-18 10:27:50 +0100

    fibonacci.sh

--- src/fibonacci.sh
+++ src/fibonacci.sh
@@ -10,14 +10,15 @@
 # Datum: 18.12.2018
 
 #globals
-syntax="./fibonacci.sh zahl"
+syntax="./fibonacci.sh"
 nurZahlen='^[0-9]+$'
 a=1
 b=0
+count=0
 
 # validation
 if [[ -n $1 ]] ; then
-    echo Richtiger Syntax: $syntax
+    echo Richtige Syntax: $syntax
     echo "Das Script fibonacci.sh, fragt nach einer Zahl berechnet und dann die entsprechenden Fibonacci-Zahlen (bei Eingabe von zB. 20 werden die ersten zwanzig Fibonacci-Zahlen ausgegeben)."
 else
     read -p "Geben Sie eine Zahl ein: " zahl
@@ -37,8 +38,11 @@ else
             b=$temp
             zahl=$((zahl-1))
 
+            # count
+            (( count++ ))
+
             # output
-            echo $zahl": " $b
+            echo "N"$count":" $b
         done
     fi
 fi
